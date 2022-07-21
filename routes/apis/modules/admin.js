13f1bbf755bd86a06b05449a8466dd5a3adadad1
Rita Chien <ritachien929@gmail.com>
2022-07-21 14:21:02 +0800

    fix: rename /controllers/apis/admin-controller.js

--- routes/apis/modules/admin.js
+++ routes/apis/modules/admin.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const adminController = require('../../../controllers/apis/admin-controllers')
+const adminController = require('../../../controllers/apis/admin-controller')
 const upload = require('../../../middleware/multer')
 
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
